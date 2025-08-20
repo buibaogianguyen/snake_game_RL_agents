3543e36a8e767193f504ed794c2e856bfb5604c3
buibaogianguyen <buibaogianguyen2008@gmail.com>
2025-08-20 14:15:41 -0400

    critic

--- model/critic.py
+++ model/critic.py
@@ -1,16 +1,18 @@
 import tensorflow as tf
 
 class Critic:
-    def __init__(self, latent_size):
-        self.latent_size = latent_size
+    def __init__(self, state_size):
+        self.state_size = state_size
         self.model = self._build_model()
 
     def _build_model(self):
         model = tf.keras.models.Sequential([
-            tf.keras.layers.Dense(64, activation='relu', input_shape=(self.latent_size,)),
+            tf.keras.layers.Dense(64, activation='relu', input_shape=(self.state_size,)),
+            tf.keras.layers.Dense(64, activation='relu'),
             tf.keras.layers.Dense(1)
         ])
+
         return model
 
-    def predict(self, latent):
-        return self.model.predict(latent, verbose=0)
+    def predict(self, state):
+        return self.model.predict(state, verbose=0)
