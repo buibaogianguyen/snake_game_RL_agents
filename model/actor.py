052cf40030fe552b9f73d68b4a21a43caed6f40c
buibaogianguyen <buibaogianguyen2008@gmail.com>
2025-08-20 14:11:32 -0400

    actor

--- model/actor.py
+++ model/actor.py
@@ -1,17 +1,19 @@
 import tensorflow as tf
 
 class Actor:
-    def __init__(self, latent_size, action_size):
-        self.latent_size = latent_size
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
         self.action_size = action_size
         self.model = self._build_model()
 
     def _build_model(self):
         model = tf.keras.models.Sequential([
-            tf.keras.layers.Dense(64, activation='relu', input_shape=(self.latent_size,)),
+            tf.keras.layers.Dense(64, activation='relu', input_shape=(self.state_size,)),
+            tf.keras.layers.Dense(64, activation='relu'),
             tf.keras.layers.Dense(self.action_size, activation='softmax')
         ])
-        return model
 
-    def predict(self, latent):
-        return self.model.predict(latent, verbose=0)
+        return model
+    
+    def predict(self, state):
+        return self.model.predict(state, verbose=0)
